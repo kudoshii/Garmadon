1ecab7104ff59ff14af0151053fd59ca5ed4a639
kudoshii <bryanmunozrodriguez7@gmail.com>
2025-11-13 16:16:23 -0300

    Implementación de Firebase al proyecto.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,8 @@ import org.gradle.api.JavaVersion
 
 plugins {
     alias(libs.plugins.android.application)
-    //id("com.google.gms.google-services")
+    //id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,21 +38,25 @@ android {
 
 dependencies {
     implementation(libs.appcompat)
-    implementation("com.google.android.material:material:1.11.0")
+    implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
 
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("androidx.cardview:cardview:1.0.0")
 
     // Firebase (se mantienen comentadas)
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
     /*implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-firestore:24.10.3")
     implementation("com.google.firebase:firebase-storage:20.3.0")*/
 
     // La librería principal (runtime)
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.github.bumptech.glide:glide:5.0.5")
+    annotationProcessor("com.github.bumptech.glide:compiler:5.0.5")
 
     // Pruebas
     testImplementation(libs.junit)
